dfcb7e6667fb9ff958cb8384ae1226040211c748
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-07-26 09:08:48 +0000

    one

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+
+  function update(e){
+    e.preventDefault();
+
+    if(input !== ""){
+      setTodos([{id: new Date(), todos: input}, ...todos]);
+    }
+  }
+
+  function del(id){
+    let filtered = todos.filter((ele) => ele.id !== id);
+    setTodos(filtered);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h3>To-Do List</h3>
+        <form onSubmit={update}>
+          <input type="text" onChange={(e) => setInput(e.target.value)} />
+          <button type="submit">Add Todo</button>
+        </form>
+
+        {
+          todos.map((ele) => 
+            <div id={ele.id}>
+              <p>{ele.todos}</p>
+              <button onClick={ () => del(ele.id)}>Delete</button>
+            </div>
+          )
+        }
+        
     </div>
   )
 }
